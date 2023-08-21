a3b074695ee46831747792cdb9f612e4e3f5656e
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-08-20 22:26:30 -0300

    chore: remove unnecessary code from install.php

--- src/install.php
+++ src/install.php
@@ -5,25 +5,6 @@ require_once 'vendor/autoload.php';
 
 use Eclesiaste\TesteTecnicoDevPhp\config\Database;
 use Eclesiaste\TesteTecnicoDevPhp\services\UserManagement;
-use Exception;
-
-// class Installation{
-//     public function execute(){
-//         try {
-//             $db = new Database();
-//             $service = new UserManagement();
-//             $db->createTables();
-//             $service->getClients();
-//             echo "Sucesso: banco de dados criado. \n";
-//         } catch (PDOException $e) {
-//             echo "Erro: problema para criar o banco de dados. \n";
-//             echo "Mensagem: " . $e->getMessage() . "\n";
-//         }
-//     }
-// }
-
-// $inst = new Installation();
-// $inst->execute();
 
 try {
     $db = new Database();
